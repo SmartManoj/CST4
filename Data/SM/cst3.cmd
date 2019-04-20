0299f0e635858ff34f63dceb6eb4c7564173c213
SmartManoj <smartmanoj42857@gmail.com>
2019-04-20 13:14:12 +0530

    'Cool'

--- Data/SM/cst3.cmd
+++ Data/SM/cst3.cmd
@@ -8,8 +8,9 @@ rem heroku accounts:set p
 git init
 git add .
 git commit -m 'Cool'
-rem git push --set-upstream https://github.com/SmartManoj/CST3 master
-rem git push origin master --force
+rem git push --set-upstream h
+ttps://github.com/SmartManoj/CST3 master
+git push origin master --force
 rem git remote add origin https://github.com/SmartManoj/CST3.git
 git push --force origin master
 rem git push -u  https://github.com/SmartManoj/CST3  --force
