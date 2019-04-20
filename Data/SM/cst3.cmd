d5fb70f381710c8bda254680f36e1adcf56f7545
SmartManoj <smartmanoj42857@gmail.com>
2019-04-20 13:14:30 +0530

    'Cool'

--- Data/SM/cst3.cmd
+++ Data/SM/cst3.cmd
@@ -10,7 +10,7 @@ git add .
 git commit -m 'Cool'
 git push --set-upstream https://github.com/SmartManoj/CST3 master
 git push origin master --force
-rem git remote add origin https://github.com/SmartManoj/CST3.git
+git remote add origin https://github.com/SmartManoj/CST3.git
 git push --force origin master
 rem git push -u  https://github.com/SmartManoj/CST3  --force
 echo %date%,%time%
