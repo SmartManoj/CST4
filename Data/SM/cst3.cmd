847c8d5947f726ebed86bc274176ce0b1f9bb8f0
SmartManoj <smartmanoj42857@gmail.com>
2019-05-31 21:52:12 +0530

    'Cool'

--- Data/SM/cst3.cmd
+++ Data/SM/cst3.cmd
@@ -1,12 +1,13 @@
 @echo off
-
-call a.cmd
-rem subl .git\info\exclude
 rem exit
 rem heroku accounts:set p
+call a.cmd
+git rm -r --cached Data\Backup
+git rm -r --cached Data\Cache
 git init
 git add .
 git commit -m 'Cool'
+exit
 rem git push --set-upstream https://github.com/SmartManoj/CST3 master
 rem git push origin master --force
 rem git remote add origin https://github.com/SmartManoj/CST3.git
