91ed9af78f63cf84eed9762d6a1e5ba441436baf
SmartManoj <smartmanoj42857@gmail.com>
2020-02-23 11:39:48 +0530

    'Cool'

--- Data/SM/cst3.cmd
+++ Data/SM/cst3.cmd
@@ -7,8 +7,8 @@ git add .
 rem git rm -r --cached Data/Backup
 rem git rm -r --cached 	Data/Cache
 git commit -m 'Cool'
-git push --set-upstream https://github.com/SmartManoj/CST3 master
-git push origin master --force
+rem git push --set-upstream https://github.com/SmartManoj/CST3 master
+git push  origin master 
 rem git remote add origin https://github.com/SmartManoj/CST3.git
 rem git push  origin master
 rem git push -u  https://github.com/SmartManoj/CST3  --force
